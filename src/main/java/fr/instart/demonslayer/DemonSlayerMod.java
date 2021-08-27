fa658cae7166e39b6471292dd4323e1176e9f01b
Tristan <tristan.fraboulet@netc.eu>
2021-08-27 12:04:25 +0200

    Création de la base du mode

--- src/main/java/fr/instart/demonslayer/DemonSlayerMod.java
+++ src/main/java/fr/instart/demonslayer/DemonSlayerMod.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package fr.instart.demonslayer;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
@@ -13,19 +13,18 @@ import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import java.util.stream.Collectors;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.stream.Collectors;
-
-// The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
+@Mod("demonslayer")
+public class DemonSlayerMod 
 {
-    // Directly reference a log4j logger.
+	
+	// Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public ExampleMod() {
+    public DemonSlayerMod() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
