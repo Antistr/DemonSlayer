4d1a1187e220c29433e20a3e3594f9535029d316
Tristan <tristan.fraboulet@netc.eu>
2021-08-28 19:02:01 +0200

    Procédure au chargement du mode

--- src/main/java/instart/demonslayer/DemonSlayerMode.java
+++ src/main/java/instart/demonslayer/DemonSlayerMode.java
@@ -1,8 +1,19 @@
 package instart.demonslayer;
 
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod("demonslayer")
 public class DemonSlayerMode {
 	
+	public DemonSlayerMode()
+	{
+	FMLJavaModLoadingContext.get().getModEventBus().addListener(this::auSetupCommun);
+	}
+	
+	private void auSetupCommun(FMLCommonSetupEvent event)
+	{
+		
+	}
 }
